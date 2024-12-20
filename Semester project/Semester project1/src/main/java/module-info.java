3ed12322511dc74d8d180806dc4766e8384de34e
Taha <SP24-BSE-088@cuilahore.edu.pk>
2024-12-20 17:20:03 +0500

    Delete Semester project/Semester project1 directory

--- Semester project/Semester project1/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.semesterrproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.semesterrproject to javafx.fxml;
-    exports com.example.semesterrproject;
-}
